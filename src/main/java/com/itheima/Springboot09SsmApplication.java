9fbe79a0740934b5e9bac097f9bacb8d9844130f
mingxxx9 <2654774823@qq.com>
2023-05-11 21:38:19 +0800

    a

--- src/main/java/com/itheima/Springboot09SsmApplication.java
+++ src/main/java/com/itheima/Springboot09SsmApplication.java
@@ -8,6 +8,9 @@ public class Springboot09SsmApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Springboot09SsmApplication.class, args);
+		System.out.println(
+				"gg"
+		);
 	}
 
 }
